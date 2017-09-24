7ce4daf83bf1bdef32b43775191aad849b72d07a
johanneshilden <hildenjohannes@gmail.com>
2017-09-24 03:25:09 +0300

    Add lens code

--- src/Data/FuzzySet/Lens.hs
+++ src/Data/FuzzySet/Lens.hs
@@ -1,1 +1,23 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Data.FuzzySet.Lens where
+
+import Control.Lens
+import Lib
+
+import qualified Data.HashMap.Strict as HashMap
+import qualified Data.Vector         as Vector
+
+makeLensesFor
+  [ ("items"           , "_items")
+  , ("matchDict"       , "_matchDict")
+  , ("exactSet"        , "_exactSet")
+  , ("useLevenshtein"  , "_useLevenshtein")
+  , ("gramSizeLower"   , "_gramSizeLower")
+  , ("gramSizeUpper"   , "_gramSizeUpper")
+  ] ''FuzzySet
+
+makeLensesFor
+  [ ("vectorMagnitude" , "_vectorMagnitude")
+  , ("normalizedEntry" , "_normalizedEntry")
+  ] ''FuzzySetItem
+
